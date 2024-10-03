76504d613ea24f2a36463cb1884ed9e460b3264c
NerissaNikmatul <157441850+NerissaNikmatul@users.noreply.github.com>
2024-10-03 17:11:41 +0700

    Dasboard1.py

--- dashboard/Dasboard1.py
+++ dashboard/Dasboard1.py
@@ -16,7 +16,7 @@ all_df.sort_values(by="order_approved_at", inplace=True)
 all_df.reset_index(inplace=True)
 
 # Geolocation Dataset
-geolocation = pd.read_csv('https://github.com/NerissaNikmatul/E-Commerce-/blob/main/geolocation.csv')
+geolocation = pd.read_csv('https://raw.githubusercontent.com/NerissaNikmatul/E-Commerce-/blob/main/geolocation.csv')
 data = geolocation.drop_duplicates(subset='customer_unique_id')
 
 for col in datetime_cols:
